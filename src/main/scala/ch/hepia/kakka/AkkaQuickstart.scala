2552cf56243127553e639de23c23a7abf7063e52
Joel Cavat <jcavat@gmail.com>
2019-12-10 22:23:19 +0100

    Improve graph with parallelism

--- src/main/scala/ch/hepia/kakka/AkkaQuickstart.scala
+++ src/main/scala/ch/hepia/kakka/AkkaQuickstart.scala
@@ -3,11 +3,12 @@ package ch.hepia.kakka
 
 import akka.NotUsed
 import akka.actor.ActorSystem
-import akka.stream.{FlowShape, Outlet}
-import akka.stream.scaladsl.{Broadcast, Flow, GraphDSL, Sink, Source, Zip}
+import akka.stream.{FlowShape, Outlet, scaladsl}
+import akka.stream.scaladsl.{Broadcast, Flow, GraphDSL, Sink, Source, Zip, ZipWith, ZipWithN}
 
 import scala.concurrent.duration._
 import scala.concurrent.duration.FiniteDuration
+import scala.util.Random
 
 object AkkaQuickstart extends App {
 
@@ -15,12 +16,18 @@ object AkkaQuickstart extends App {
   implicit val sys = ActorSystem("MyTest")
 
 
-  val source: Source[Int, NotUsed] = Source(1 to 1000)
-  val windowedShort: Flow[Int, Seq[Int], NotUsed] = Flow[Int].sliding(100).map( _.takeRight(10) )
-  val windowedLong: Flow[Int, Seq[Int], NotUsed] = Flow[Int].sliding(100)
+  val source: Source[Int, NotUsed] = Source(LazyList.continually(new Random().nextInt(100)).take(1000))
+  val maxChunk: Int = 30
+
+  def chunk(n: Int): Flow[Int, Seq[Int], NotUsed] = Flow[Int].sliding(n)
+  def chunked: Flow[Int, Seq[Int], NotUsed] = chunk(maxChunk)
+
+  val windowedShort: Flow[Int, Seq[Int], NotUsed] = chunked.map( _.takeRight(10) )
+  val windowedLong: Flow[Int, Seq[Int], NotUsed] = chunked
+
+  val sink = Sink.foreach[String]( println )
 
   val mean: Flow[Seq[Int], Double, NotUsed] = Flow[Seq[Int]].map( s => {
-    println(s)
     s.sum.toDouble / s.length.toDouble
   } )
 
@@ -32,7 +39,6 @@ object AkkaQuickstart extends App {
       """.stripMargin
   }
 
-
 //  source.via(windowedShort).runForeach( seqs => println(seqs) )
 
   val graph = Flow.fromGraph(GraphDSL.create() {
@@ -40,16 +46,16 @@ object AkkaQuickstart extends App {
       import GraphDSL.Implicits._
 
       val broadcast = builder.add(Broadcast[Int](2))
-      val zip = builder.add(Zip[Double,Double]())
+      val zip = builder.add(ZipWith[Double,Double, (Double,Double)]{ case (a,b) => (a,b)})
       val ms = builder.add(Flow[String])
 
-      broadcast.out(0) ~> windowedShort ~> mean ~> zip.in0
-      broadcast.out(1) ~> windowedLong ~> mean ~> zip.in1
+      broadcast.out(0) ~> windowedShort.via(mean).async ~> zip.in0
+      broadcast.out(1) ~> windowedLong.via(mean).async ~> zip.in1
       zip.out ~> means ~> ms
 
       FlowShape(broadcast.in, ms.out)
 
   })
-  graph.runWith(source, Sink.foreach[String]( println ))
+  source.via(graph).runWith(sink)
 
 }
